074e45e5a2d68b89e24a4262dcc6d5578db26539
Davit1996Sargsyan <sargsyandavitgithub@gmail.com>
2025-02-15 16:52:18 +0400

    Add a logic to check the presence of requesting link in BlackListDB

---
+++ AntiPhishingAPI/Data/DTO/CheckingLink.cs
@@ -0,0 +1,8 @@
+﻿namespace AntiPhishingAPI.Data.DTO
+{
+    public class CheckingLink
+    {
+        public string Link { get;set; }
+        public double Dangerousity { get; set; } = default(double);
+    }
+}
